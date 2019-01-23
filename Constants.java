7756bc507db679cbbd7b6544e46ae8ad9647d9bf
Connor Brown <downtown.c.brown1999@gmail.com>
2019-01-23 14:45:21 -0500

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -3,7 +3,17 @@ package grid;
 
 public class Constants {
 
-	public static final int ROOT_WIDTH = 800;
-	public static final int ROOT_HEIGHT = 496;
+	public static final double ROOT_WIDTH = 967.74;
+	public static final double ROOT_HEIGHT = 600;
 	public static final double DURATION = .5;
+	public static final int HB_WIDTH = 700;
+	public static final int HB_HEIGHT = 396;
+	public static final int HB_X = 134;
+	public static final int HB_Y = 102;
+	public static final String[] HELP_STRINGS = { "type ESC to hide the help box or H to display it again.",
+			"	◘ type ENTER to generate the grid and DELETE to clear the canvas",
+			"	◘ type R, B, Y to toggle the selected color, try clicking on sub-regions of the canvas :)",
+			"	◘ type command-O to select a background image", "	◘ type shift-G to make the background image B&W",
+			"	◘ type command-S to save your masterpiece",
+			"	◘ hold down shift-D to compare your work to the original background image" };
 }
